06295e87d8753661b0122d77cd82b98835bc40fb
Minsuuu <sd15615@gmail.com>
2018-04-11 20:16:56 +0900

    boolean

---
+++ 4_Boolean.py
@@ -0,0 +1,18 @@
+print(10 == 100)
+print(10 != 100)
+print(10 < 100)
+print(10 > 100)
+print(10 <= 100)
+print(10 >= 100)
+print()
+
+print("가방" == "하마")
+print("가방" != "하마")
+print("가방" < "하마")
+print("가방" > "하마")
+print()
+
+x=25
+print(10 < x < 30)
+print(40 < x < 60)
+print()
