9959614d986467fefd7d51ce73e697d6dd476813
Minsuuu <sd15615@gmail.com>
2019-05-29 20:03:39 +0900

    190529_PLY

---
+++ 계산이론/2-ply_ex.py
@@ -0,0 +1,61 @@
+# plyexample.py
+#
+# Example of parsing with PLY
+
+from ply.lex import lex
+
+# Token list
+tokens = [ 'NUM', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN', 'ID', 'HEX']
+
+# Ignored characters
+
+t_ignore = ' \t\n'
+
+# Token specifications (as regexs)
+t_PLUS   = r'\+'
+t_MINUS  = r'-'
+t_TIMES  = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+
+# Token processing functions
+def t_HEX(t):
+    r'[0][x][0-9a-f]+'
+    t.value = int(t.value, 16)
+    return t
+
+def t_NUM(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_ID(t):
+    r'[a-zA-Z][a-zA-Z_0-9]*'
+    # define a Symbol Table
+    return t
+
+
+
+# Error handler
+def t_error(t):
+    print('Bad character: {!r}'.format(t.value[0]))
+    t.skip(1)
+
+# Build the lexer
+lexer = lex()
+
+# Test it out
+data = '''
+3 + 4 * 10  + -20 *2  0x10 0x1f 0x1a
+'''
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok: 
+        break      # No more input
+    print(tok)
